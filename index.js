69ddcff049529cbe4fe90a21b70233fb07a4a0cc
Benson Lindsey <blthree89@gmail.com>
2017-12-13 22:57:56 -0500

    added idea for function mapping

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 
+// will probably be easiest to have a single update button and function
+// that will check each input element for changes and call a css update function 
+// that is specific to each css attribute
+// below is what I'm imagining:
+var divToFunctionMap = {"color-picker": 
+                            {"value": "white",
+                             "updateFunction": updateBackgroundColor,
+                            }
+                       }
+
 function updateBackgroundColor () {
     if(colorPicker.value.length === 6) {
         myBackgroundColor = "#" + colorPicker.value;
@@ -13,9 +23,14 @@ function updateBackgroundColor () {
     return;
 }
 
+// the <div> element showing previews of css changes
 var theDiv = document.querySelector("#the-div");
+// color input box
 var colorPicker = document.querySelector("#color-picker");
+// update color button
 var addBackgroundColorButton = document.querySelector("#add-background-color");
+// default background color
 var myBackgroundColor = "white";
+// event listener for color update button
 addBackgroundColorButton.addEventListener("click", updateBackgroundColor);
 
